68bb74c72b8e0609107db1f163578fa0d5a567a9
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-12 15:10:17 -0800

    commented Arduino code

--- Trivia.ino
+++ Trivia.ino
@@ -1,3 +1,4 @@
+//Define variables
 #define redLight 2
 #define greenLight 3
 #define button1 4
@@ -5,45 +6,48 @@
 #define button3 6
 
 void setup() {
-  // put your setup code here, to run once:
+  //Setup button pins as input, and LED pins as output
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
   pinMode(button3, INPUT);
   pinMode(redLight, OUTPUT);
   pinMode(greenLight, OUTPUT);
+  //Begin the Serial
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+
+  //if button one is pressed (option A)  
   if(digitalRead(button1) == HIGH){
-    Serial.println("1");
+    Serial.println("1"); //send 1 to Processor through Serial
   }
 
+  //if button two is pressed (option B)  
   if(digitalRead(button2) == HIGH){
-    Serial.println("2");
+    Serial.println("2"); //send 2 to Processor through Serial
   }
 
+//if button two is pressed (option C)
   if(digitalRead(button3) == HIGH){
-    Serial.println("3");
+    Serial.println("3"); //send 3 to Processor through Serial
   }
 
   if (Serial.available ( ) > 0) {   // Checking if the Processing IDE has send a value or not
     char state = Serial.read ( );    // Reading the data received and saving in the state variable
     
-    if(state == '1') { 
+    if(state == '1') { // if guess was correct, turn the green LED on for a second
       digitalWrite (greenLight, HIGH); 
       delay(1000);
       digitalWrite (greenLight, LOW); 
     }  
 
-    if (state == '0') {     // If received data is '0', then turn off led
+    if (state == '0') { // if guess was incorrect, turn the red LED on for a second
      digitalWrite (redLight, HIGH);
      delay(1000);
      digitalWrite (redLight, LOW); 
     } 
 } 
 
-  delay(500);
+  delay(500); //wait half a second intervals
 }
